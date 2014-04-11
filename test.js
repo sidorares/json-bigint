bbae6f2a7076c1a17ea8e7615b18d1820b8b71ad
Timo Kissing <timo.kissing@citrix.com>
2014-04-11 12:38:20 -0700

    Add missing semicolon

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var json = '{ "value" : 9223372036854775807, "v2": 123 }';
 console.log('Input:', json);
 console.log('');
 
-console.log('node.js bult-in JSON:')
+console.log('node.js bult-in JSON:');
 var r = JSON.parse(json);
 console.log('JSON.parse(input).value : ', r.value.toString());
 console.log('JSON.stringify(JSON.parse(input)):', JSON.stringify(r));
