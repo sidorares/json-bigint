ae6642e1ee3191db089b4ff0cecd9c9f9b150dc3
chris_council <chris.council@airbnb.com>
2019-01-08 13:48:46 -0800

    Added named export for better TypeScript compatibility

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var json_stringify = require('./lib/stringify.js').stringify;
 var json_parse     = require('./lib/parse.js');
 
-module.exports = function(options) {
+var newInstance = function(options) {
     return  {
         parse: json_parse(options),
         stringify: json_stringify
     }
 };
+
+module.exports = newInstance;
+
 //create the default method members with no options applied for backwards compatibility
 module.exports.parse = json_parse();
 module.exports.stringify = json_stringify;
+
+// for cleaner typescript API
+module.exports.newInstance = newInstance;
