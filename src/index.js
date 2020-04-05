545efb74f59712d06532514cdf7d648a25455d7a
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-04-05 15:17:37 +0300

    0.0.2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import { stringify } from './lib/stringify';
 import { json_parse } from './lib/parse'
 
-globalThis.JSON.parse = json_parse();
-globalThis.JSON.stringify = stringify;
+let globalObj;
+
+if (typeof globalThis !== 'undefined') {
+    globalObj = globalThis;
+} else if (typeof self !== 'undefined') {
+    globalObj = self;
+} else if (typeof window !== 'undefined') {
+    globalObj = window;
+} else if (typeof global !== 'undefined') {
+    globalObj = global;
+}
+
+globalObj.JSON.parse = json_parse();
+globalObj.JSON.stringify = stringify;
